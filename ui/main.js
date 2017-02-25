55d023f8d96587c2dd7caadb45ab3ebabe5a3772
shweta007 <me.shweta007@gmail.com>
2017-02-25 23:58:25 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,5 +1,5 @@
 console.log('Loaded!');
 
 
-var para=document.getElementById('p1');
-para.innerHTML='this is my document.';
+var paraa=document.getElementById('p1');
+paraa.innerHTML='this is my document.';
