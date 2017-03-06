741c1cc1272ed016a229a882ac96e9cf275bcf02
shweta007 <me.shweta007@gmail.com>
2017-03-06 23:33:39 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,3 +3,13 @@ console.log('Loaded!');
 
 var paraa=document.getElementById('p1');
 paraa.innerHTML ='this is my document';
+
+var xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+       // Typical action to be performed when the document is ready:
+       document.getElementById("ctext").innerHTML = xhttp.responseText;
+    }
+};
+xhttp.open("GET", "filename", true);
+xhttp.send();
