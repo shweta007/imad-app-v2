44b2a50083c6e6d37a22bce91e063932618a912a
shweta007 <me.shweta007@gmail.com>
2017-03-08 21:51:51 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,12 +4,22 @@ console.log('Loaded!');
 var paraa=document.getElementById('p1');
 paraa.innerHTML ='this is my document';
 
+
+var btn=document.getElementById("btnsubmit");
+ btn.onclick=function()
+ {
+
 var xhttp = new XMLHttpRequest();
 xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
        // Typical action to be performed when the document is ready:
-       document.getElementById("ctext").innerHTML = xhttp.responseText;
+       var text = request.responseText;
+      var holdtext= document.getElementById("ctext");
+      
+      holdtext.innerHTML=text.toString();
+      
     }
 };
-xhttp.open("GET", "filename", true);
+};
+xhttp.open("GET", "http://shweta007.imad.hasura-app.io/ui/comment", true);
 xhttp.send();
