5991619b543207f6207921f77d0eb96650428696
shweta007 <me.shweta007@gmail.com>
2017-03-08 21:58:02 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,8 +1,8 @@
 console.log('Loaded!');
 
 
-var paraa=document.getElementById('p1');
-paraa.innerHTML ='this is my document';
+/*var paraa=document.getElementById('p1');
+paraa.innerHTML ='this is my document';*/
 
 
 var btn=document.getElementById("btnsubmit");
