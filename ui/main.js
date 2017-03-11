4f7d9ba0f6e16efbd862fdd3acf97f8c03b2c0b7
shweta007 <me.shweta007@gmail.com>
2017-03-11 19:34:09 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -9,17 +9,17 @@ var name=nameInput.value;
 var btn=document.getElementById("btnsubmit");
  btn.onclick=function()
  {
-//var request= new XMLHttpRequest();
+var request= new XMLHttpRequest();
 
-//request.onreadystatechange = function() {
-    //if (request.readyState == XMLHttpRequest.DONE) {
+request.onreadystatechange = function() {
+    if (request.readyState == XMLHttpRequest.DONE) {
         
-        //if(request.status===200){
+        if(request.status===200){
        // Typical action to be performed when the document is ready:
-      // var names = request.responseText;
-      // names=JSON.parse(names);
+       var names = request.responseText;
+       names=JSON.parse(names);
        var list='';
-       var names=['name1','name2'];
+      // var names=['name1','name2'];
        for(var i=0; i<names.length;i++)
        {
           list += '<br>' + '<span>' + names[i] + '</span>' + '<br>';
@@ -27,11 +27,11 @@ var btn=document.getElementById("btnsubmit");
       var ul= document.getElementById("holdtext");
       
       ul.innerHTML=list;
-        //}
-    //}
+        }
+    }
 
-//};
+};
 
-//request.open('GET', 'http://shweta007.imad.hasura-app.io/ui/comment?name='+name , true);
-//request.send(null);
+request.open('GET', 'http://shweta007.imad.hasura-app.io/ui/comment?name='+name , true);
+request.send(null);
 };
