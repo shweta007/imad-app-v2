cdedaf7ad935a44b51412060d2a08e3db8a1eb06
shweta007 <me.shweta007@gmail.com>
2017-03-11 18:55:42 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(morgan('combined'));
 
 var names=[];
 app.get('/comment',function(req,res){
-    var name=req.params.name;
+    var name=req.query.name;
     names.push(name);
     res.send(JSON.stringify(names));
 });
