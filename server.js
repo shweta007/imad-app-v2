08ca1b7247cbf169cc5cc4dc4a4a17e01b547b75
shweta007 <me.shweta007@gmail.com>
2017-03-16 23:19:46 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -110,6 +110,23 @@ app.get('/:articleName',function(req,res){
   res.send(createTemplate(articles[articleName]));
 });
 
+app.get('/articles/:articleName',function(req,res)
+{
+    pool.query("SELECT * FROM article1 where title= ' " + req.params.articleName+ " ' ",function(err,result){
+        
+        if(err){
+            res.status(500).send(err.toString());
+        }else {
+            if(result.rows.length===0){
+                res.status(404).send('article not found');
+            } else{
+                var articleData=result.rows[0];
+                res.send(createTemplate(articleData));
+            }
+        }
+    });
+});
+
 app.get('/article-one',function(req,res){
     res.send('this is my article one any where it is getting' );
 });
