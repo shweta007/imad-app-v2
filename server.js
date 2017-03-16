0bf62be6c16002766e08328d99aacc30a6afeb2c
shweta007 <me.shweta007@gmail.com>
2017-03-16 15:39:21 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ var config={
     database:'shweta007',
     host:'db.imad.hasura-app.io',
     port:'5432',
-    password:process.env.db_shweta007-4023
+    password:process.env.DB_PASSWORD
     
 };
 
