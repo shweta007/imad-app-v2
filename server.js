b186d99a83ea84ee4d9770df35ea9468bd4e7747
shweta007 <me.shweta007@gmail.com>
2017-03-18 17:17:04 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var crypto=require('crypto');
 var app = express();
 app.use(morgan('combined'));
+
 
 var Pool=require('pg').Pool;
 var config={
