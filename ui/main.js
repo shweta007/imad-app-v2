cb54b4f8329026c9a19510f3d2cf768d8b4a16a4
shweta007 <me.shweta007@gmail.com>
2017-03-19 00:59:14 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -12,24 +12,40 @@ var request= new XMLHttpRequest();
 
 request.onreadystatechange = function() {
     if (request.readyState == XMLHttpRequest.DONE) {
-        
+        //take action
         if(request.status===200){
+            alert("logged in successfully");
+        }else if(request.status==403)
+        {alert("user name/password is incorrect");
+        } else if(request.status==500){
+            alert("something went wrong on server");
+        }
+        }
+};
+        
+        
+        
        // Typical action to be performed when the document is ready:
-       var names = request.responseText;
-       names=JSON.parse(names);
+       //var names = request.responseText;
+       //names=JSON.parse(names);
 // var list;
 //var names=['name1','name2'];
    //   for(var i=0; i<names.length;i++)
 //
-      var div= document.getElementById("holdtext");
+  //    var div= document.getElementById("holdtext");
       
-      div.innerHTML=names;
-        }
-    }
+    //  div.innerHTML=names;
+      //  }
+    //}
 
-};
-var nameInput= document.getElementById("ctext");
-var name=nameInput.value;
-request.open('GET', 'http://shweta007.imad.hasura-app.io/comment?name='+ name , true);
-request.send(null);
+//};
+//var username= document.getElementById("ctext");
+//var name=nameInput.value;
+var username = document.getElementById("username").value;
+var password = document.getElementById("password").value;
+console.log(username);
+console.log(password);
+request.open('post', 'http://shweta007.imad.hasura-app.io/login' , true);
+request.send('Content-Type','application-json');
+request.send(JSON.stringify({username:username,password:password})); 
 };
