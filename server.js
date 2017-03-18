e8a467d64a3cc621d0e04205c734d57e57cec670
shweta007 <me.shweta007@gmail.com>
2017-03-19 01:13:36 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -218,8 +218,8 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
-app.get('imad-app-v2/ui/shweta.jpg', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'shweta.jpg'));
+app.get('/ui/madi.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
 
