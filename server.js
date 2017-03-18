8b16ce336361cdf55ddb0fde1ae887253684d264
shweta007 <me.shweta007@gmail.com>
2017-03-18 15:22:18 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -112,7 +112,7 @@ app.get('/:articleName',function(req,res){
 
 app.get('/articles/:articleName',function(req,res)
 {
-    pool.query("SELECT * FROM article1 WHERE title= ' " + req.params.articleName+ "'",function(err,result){
+    pool.query("SELECT * FROM article1 WHERE title= '" + req.params.articleName+ "'",function(err,result){
         
         if(err){
             res.status(500).send(err.toString());
