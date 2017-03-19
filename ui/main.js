5cda60009af0756d04f185967311e159d2797db5
shweta007 <me.shweta007@gmail.com>
2017-03-19 21:54:33 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -47,5 +47,5 @@ console.log(username);
 console.log(password);
 request.open('POST', 'http://shweta007.imad.hasura-app.io/login' , true);
 request.setRequestHeader('Content-Type','application-json');
-request.send(JSON.stringify({username:username,password:password})); 
+request.send(JSON.stringify({username:username})); 
 };
